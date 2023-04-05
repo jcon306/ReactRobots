056dc2d74126fc72dea11214ad66182c1f3fa740
Joe C <consolinojoe@gmail.com>
2023-04-05 13:36:25 -0400

    Style updates

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import CardList from './CardList'
 import { robots } from './robots';
 import SearchBar from './SearchBar'
-
+import './App.css'
 
 class  App extends Component{
     constructor() {
@@ -24,8 +24,8 @@ class  App extends Component{
             return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
         })
         return (
-            <div className='tc bg-blue'>
-                <h1>RoboFriends</h1>
+            <div className='tc'>
+                <h1 className='f1'>RoboFriends</h1>
                 <SearchBar searchChange={this.onSearchChange}/>
                 <CardList robots={filteredRobots}/>
             </div>
