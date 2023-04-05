7b572f86f411a38fe4fad9188a0f6684d0bd7110
Joe C <consolinojoe@gmail.com>
2023-04-05 14:47:37 -0400

    Added fetch method to retrieve data from the JSON placeholder API. Also added new scroll component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react'
 import CardList from './CardList'
-import { robots } from './robots';
 import SearchBar from './SearchBar'
+import Scroll from './Scroll.js'
 import './App.css'
 
 class  App extends Component{
     constructor() {
         super()
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => this.setState({ robots: users}))
     }
 
     onSearchChange = (e) => {
@@ -23,13 +29,19 @@ class  App extends Component{
         const filteredRobots = this.state.robots.filter(robot => {
             return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
         })
-        return (
-            <div className='tc'>
-                <h1 className='f1'>RoboFriends</h1>
-                <SearchBar searchChange={this.onSearchChange}/>
-                <CardList robots={filteredRobots}/>
-            </div>
-        )
+        if (this.state.robots.length === 0) {
+            return <h1>LOADING...</h1>
+        } else {
+            return (
+                <div className='tc'>
+                    <h1 className='f1'>RoboFriends</h1>
+                    <SearchBar searchChange={this.onSearchChange}/>
+                    <Scroll>
+                        <CardList robots={filteredRobots}/>
+                    </Scroll>
+                </div>
+            )
+        }     
     }
 }
 
