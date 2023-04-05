f7eda3698c686d684232e5cc87f06b07e4e52769
Joe C <consolinojoe@gmail.com>
2023-04-05 09:50:31 -0400

    Added filter functionality

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
-import React from 'react'
+import React, { Component } from 'react'
 import CardList from './CardList'
 import { robots } from './robots';
 import SearchBar from './SearchBar'
 
-const App = () => {
-    return (
-        <div className='tc'>
-            <h1>RoboFriends</h1>
-            <SearchBar />
-            <CardList robots={robots}/>
-        </div>
-    )
+
+class  App extends Component{
+    constructor() {
+        super()
+        this.state = {
+            robots: robots,
+            searchfield: ''
+        }
+    }
+
+    onSearchChange = (e) => {
+        this.setState({
+            searchfield: e.target.value
+        })
+    }
+
+    render() {
+        const filteredRobots = this.state.robots.filter(robot => {
+            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+        })
+        return (
+            <div className='tc bg-blue'>
+                <h1>RoboFriends</h1>
+                <SearchBar searchChange={this.onSearchChange}/>
+                <CardList robots={filteredRobots}/>
+            </div>
+        )
+    }
 }
 
 export default App
--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,16 +1,17 @@
 import React from 'react'
 
-const SearchBox = () => {
+const SearchBar = ({ searchChange }) => {
     return (
         <div className='pa2'>
             <input 
                 className='pa3 ba b--green bg-lightest-blue'
                 type='search'
                 placeholder='search friends'
+                onChange={searchChange}
             />
         </div>
     )
 }
 
 
-export default SearchBox
+export default SearchBar
